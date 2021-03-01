368ba884801e960a3e76d3479decdb0378fa5639
dst27 <72784348+dst27@users.noreply.github.com>
2021-03-01 14:53:34 +0530

    pset1/mario/less - part rewrite
    Contrained height to [1, 8] as per 2021 spec
    Improved comments and styling

--- pset1/mario_less/mario.c
+++ pset1/mario_less/mario.c
@@ -1,38 +1,57 @@
-#import <stdio.h>
-#import <cs50.h>
+#include <stdio.h>
+#include <cs50.h>
+
+// Function Prototypes
 void stair(int n);
-int get_positive_int(void);
+int get_positive_int(int lower, int upper);
 
 int main(void)
 {
-    int n = get_positive_int();
-    stair(n);
+    // Get as input a positive int
+    int h = get_positive_int(1, 8);
+
+    // Call stair() to print stair of height h
+    stair(h);
     return 0;
 }
 
-/* implementing the staircase,  2 nested for() loops, one that does the n rows and the next that takes care of the
-   n-m " " and the m "#" in each row */
+/* **************************************
+ * Implementing the staircase using nested for() loops.
+ * One does the n rows and the 2 nested ones that take 
+ * care of the n-m spaces " " and the m sharps "#" in 
+ * each row 
+ * *************************************/
+
 void stair(int n)
 {
     int k, l, m;
-    for (m = 0; m < n; m++) // iterates n rows
+
+    // Iterates n times
+    for (m = 0; m < n; m++)
     {
-        for (k = 0; k < n - (m + 1); k++) // this for() loop takes care of the n-m " " each row
+        // Takes care of the n-m " " on each row
+        for (k = 0; k < n - (m + 1); k++)
         {
             printf(" ");
         }
-        for (l = 0; l < m + 1; l++) // this for() loop takes care of the l=m "#" each row
+
+        // Takes care of the l=m "#" each row
+        for (l = 0; l < m + 1; l++)
         {
             printf("#");
         }
+
+        // Print a newline to goto next line
         printf("\n");
     }
 }
 
-int get_positive_int(void)
+int get_positive_int(int lower, int upper)
 {
     int n = 0;
-    while (n < 1)
+
+    // Keep asking for input until a 1 <= n <= 8 
+    while (n < lower || n > upper)
     {
         n = get_int("Staircase height: ");
     }
