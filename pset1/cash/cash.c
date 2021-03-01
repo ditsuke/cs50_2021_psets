98c49fa9c226958945f1d787b5ba724114d75ca1
dst27 <72784348+dst27@users.noreply.github.com>
2021-03-01 15:42:07 +0530

    pset1/cash - part rewrite
    Improved readability and flow of logic.
    Renamed vars, removed redundant ones.
    Improved comments and styling.

--- pset1/cash/cash.c
+++ pset1/cash/cash.c
@@ -2,35 +2,40 @@
 #include <math.h>
 #include <cs50.h>
 
-float get_cash_input(void);
-
+int get_cash_input(void);
 
 int main(void)
 {
-    float n = get_cash_input();
-    int nr = (int)n;
-    //printf("Pennies owed, rounded = %i\n", nr); //comment out for minimum output
-    int a, b, c, d, r;
-    a = (int)(nr/25), r = nr%25;
-    b = (int)(r/10), r = r%10;
-    c = (int)(r/5), r = r%5;
-    d = r;
-    //printf("Minimum number of coins to pay = %i (%i, %i, %i, %i)\n", a+b+c+d, a, b, c, d); //comment out for mimimum output
-    printf("%i\n", a+b+c+d); //outputs only the minumum number and a newline character
+    // Stores dollar input converted to cents rounded to the nearest penny
+    int n_round = get_cash_input();
+    
+    // c_x stores number of coins of value 'x' cents
+    int c_25, c_10, c_5, c_1;
+    c_1 = n_round;
+
+    c_25 = c_1 / 25, c_1 %= 25;
+    c_10 = c_1 / 10, c_1 %= 10;
+    c_5 = c_1 / 5, c_1 %= 5;
+
+    // Print the minimum number of coins as computed
+    printf("%i\n", c_25 + c_10 + c_5 + c_1);
     return 0;
 }
 
-
-
-
-
-float get_cash_input(void)
+int get_cash_input(void)
 {
+    // Stores input in pennies
     float n = 0;
-    while(n<=0)
+
+    // Keep asking for input until n > 0 is entered
+    while (!(n > 0))
     {
-        n = get_float("Cash owed: "); //dollars
-        n = round(n*100); //cents rounded to nearest penny
+        // Input in dollars
+        n = get_float("Cash owed: ");
+
+        // Convert to Cents, round to nearest penny
+        n = round(n * 100);
     }
+
     return n;
 }
