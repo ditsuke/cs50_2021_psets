f611bad349454e9b9d409444f92cfa696ebd205a
dst27 <72784348+dst27@users.noreply.github.com>
2021-03-01 14:25:53 +0530

    pset1/credit - part rewrite
    Switched to using int64_t from <stdint.h> for cross-platform compatibility.
    Rewrote some code for better readability and correctness.
    Added documentation to some parts that were missing it.

--- pset1/credit/credit.c
+++ pset1/credit/credit.c
@@ -1,80 +1,108 @@
-#include <stdio.h>
 #include <math.h>
-#include <cs50.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdio.h>
 
-int DigitCount(long n);
-int LuhnCheck(long n);
+typedef int64_t longint;
 
+int DigitCount(longint n);
+bool LuhnCheck(longint n);
 
 int main(void)
 {
-    long n = get_long("Card Number: ");
-    long i_2_15 = (int)(n / (long int)pow(10, 13));
-    long i_2_16 = (int)(n / (long int)pow(10, 14));
-    long i_1_16 = (int)(n / (long int)pow(10, 15));
-    long i_1_13 = (int)(n / (long int)pow(10, 12));
+    // Input card number
+    longint n;
 
-    if (n < 0 || (DigitCount(n) != 16 && DigitCount(n) != 15 && DigitCount(n) != 13)
-        || LuhnCheck(n) == 1)     // Only allows 13, 15 and 16-digit positive numbers that follow Luhn's Algorith
+    printf("Card Number: ");
+    scanf("%lld", &n);
+
+    // Digit count for n
+    int dc = DigitCount(n);
+    // First 2 digits of 15 digit number
+    int i_2 = (n / (longint)pow(10, dc - 2));
+    // First 2 digits of 16 digit number
+    int i_1 = (longint)(n / (longint)pow(10, dc - 1));
+
+    // Only allow 13, 15 and 16-digit positive numbers
+    if (!(n > 0 && (dc == 16 || dc == 15 || dc == 13)))
     {
         printf("INVALID\n");
         return 0;
     }
-    else if (i_2_15 == 34 || i_2_15 == 37) // CHECK FOR AMEX CARDS
+    bool lc = LuhnCheck(n);
+    // Only allow Luhn compliant numbers
+    if (lc == false)
+    {
+        printf("INVALID\n");
+        return 0;
+    }
+    // CHECK FOR AMEX CARDS
+    else if (i_2 == 34 || i_2 == 37)
     {
         printf("AMEX\n");
         return 0;
     }
-    else if (i_2_16 == 51 || i_2_16 == 52 || i_2_16 == 53 || i_2_16 == 54 || i_2_16 == 55) // CHECK FOR MASTERCARD CARDS
+    // CHECK FOR MASTERCARD CARDS
+    else if (i_2 == 51 || i_2 == 52 || i_2 == 53 || i_2 == 54 || i_2 == 55)
     {
         printf("MASTERCARD\n");
         return 0;
     }
-    else if (i_1_13 == 4 || i_1_16 == 4) // CHECK FOR VISA CARDS
+    // CHECK FOR VISA CARDS
+    else if (i_1 == 4 || i_1 == 4)
     {
         printf("VISA\n");
         return 0;
     }
-    else // REJECT ALL OTHER CARDS
+    // REJECT ALL OTHER CARDS
+    else
     {
         printf("INVALID\n");
     }
-    //printf("DigiCount: %i\n", DigitCount(n));
-    //printf("Luhn: %i\n", LuhnCheck(n));
 }
 
-
-
-int DigitCount(long n) // function counts and returns number of digits in number
+// Returns number of digits in longint n
+int DigitCount(longint n)
 {
+    // Digit count
     int m = 0;
-    double t = 0;
-    while (n / (long int)pow(10, m) >= 10)
+
+    // Increment m until n / 10^m < 10
+    while (n / (longint)pow(10, m) >= 1)
     {
         m++;
     }
-    return m + 1;
+    return m;
 }
 
-int LuhnCheck(long n) // function checks whether number is compliant with Luhn's Algorithm
+// Checks for compliance of Luhn's Algorithm
+bool LuhnCheck(longint n)
 {
-    long os = 0, e2xadigi = 0, p, s;
-    for (p = 1; p <= 16; p = p + 2) //calculates the sum of the odd placed digits
+    int odd_sum = 0, evenx2_sum = 0;
+    int dc = DigitCount(n);
+
+    // Sum of the odd placed digits (from end)
+    for (longint p = 1, m = n; p <= 16; p = p + 2)
     {
-        os = os + (n % (long int)pow(10, p)) / ((long int)pow(10, p - 1));
+        odd_sum += (n % (longint)pow(10, p)) / ((longint)pow(10, p - 1));
     }
 
-    for (p = 2; p <= 16; p = p + 2) // calculates the sum of the digits of even placed digits
+    // Sum of digits of 2x the even placed digits (from end)
+    for (int p = 2; p <= 16; p = p + 2)
     {
-        e2xadigi = e2xadigi + (2 * ((n % (long int)pow(10, p)) / ((long int)pow(10, p - 1)))) % 10 + (int)(2 * ((n % (long int)pow(10
-                   , p)) / ((long int)pow(10, p - 1)))) / 10;
+        evenx2_sum +=
+            (2 * ((n % (longint)pow(10, p)) / ((longint)pow(10, p - 1)))) % 10 +
+            (int)(2 * ((n % (longint)pow(10, p)) / ((longint)pow(10, p - 1)))) /
+            10;
     }
-    if ((os + e2xadigi) % 10 != 0)
+    // Return true if odd_sum + evenx2_sum end with 0 (% 10 == 0)
+    if ((odd_sum + evenx2_sum) % 10 == 0)
     {
-        return 1;
+        return true;
     }
+    // Return false otherwise
     else
     {
-        return 0;
+        return false;
     }
 }
