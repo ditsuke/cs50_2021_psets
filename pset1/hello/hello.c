4a3bd55e67039cbb716c7e291c1dad4f1c3a0bba
dst27 <72784348+dst27@users.noreply.github.com>
2021-03-02 12:13:15 +0530

    pset1/hello - update to 2021 spec
    Didn't ask for name before, now asks for name and prints " Hello name\

--- pset1/hello/hello.c
+++ pset1/hello/hello.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
+#include <cs50.h>
 
 int main(void)
 {
+    // Prompt for name, store in in_name
+    string in_name = get_string("What is your name?");
+
     // Print to stdout
-    printf("Hello, World!\n");
+    printf("hello, %s\n");
     
     // Return with exit code 0
     return 0;
