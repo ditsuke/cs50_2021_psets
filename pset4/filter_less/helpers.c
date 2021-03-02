2a7ed6158fdb37136c8012b8bac01273169e2a94
dst27 <72784348+dst27@users.noreply.github.com>
2021-03-02 11:11:35 +0530

    pset4/filter/less - part rewrite
    Improved comments and styling.

--- pset4/filter_less/helpers.c
+++ pset4/filter_less/helpers.c
@@ -3,6 +3,7 @@
 #include <math.h>
 #include <stdbool.h>
 
+// Utility macro to swap values of subpixel intensities
 #define swap_e(a, b) \
     {                \
         a += b;      \
@@ -11,13 +12,18 @@
     }
 
 // Convert image to grayscale
-void grayscale(int height, int width, RGBTRIPLE image[height][width]) {
-    for (int i = 0; i < height; i++) {
-        for (int j = 0; j < width; j++) {
-            unsigned char rgbtGreyscale =
-                round((image[i][j].rgbtRed + image[i][j].rgbtGreen +
-                       image[i][j].rgbtBlue) /
-                      3.0);
+void grayscale(int height, int width, RGBTRIPLE image[height][width])
+{
+    // Iterate over each row of pixels
+    for (int i = 0; i < height; i++)
+    {
+        // Iterate over each pixel in row
+        for (int j = 0; j < width; j++)
+        {
+            // For greyscale, rgb values are average of pixel intensities of each color rounded to nearest int
+            unsigned char rgbtGreyscale = round((image[i][j].rgbtRed + 
+                                                 image[i][j].rgbtGreen + 
+                                                 image[i][j].rgbtBlue) / 3.0);
 
             image[i][j].rgbtRed = rgbtGreyscale;
             image[i][j].rgbtGreen = rgbtGreyscale;
@@ -28,20 +34,26 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width]) {
 }
 
 // Convert image to sepia
-void sepia(int height, int width, RGBTRIPLE image[height][width]) {
-    for (int i = 0; i < height; i++) {
-        for (int j = 0; j < width; j++) {
+void sepia(int height, int width, RGBTRIPLE image[height][width])
+{
+    // Iterate over each row of pixels
+    for (int i = 0; i < height; i++)
+    {
+        // Iterate over each pixel in row
+        for (int j = 0; j < width; j++)
+        {
+            // Store sepia values
             int sepiaRed, sepiaGreen, sepiaBlue;
-            sepiaRed = round(0.393 * image[i][j].rgbtRed +
-                             0.769 * image[i][j].rgbtGreen +
+
+            // Compute sepia values
+            sepiaRed = round(0.393 * image[i][j].rgbtRed + 0.769 * image[i][j].rgbtGreen + 
                              0.189 * image[i][j].rgbtBlue);
-            sepiaGreen = round(0.349 * image[i][j].rgbtRed +
-                               0.686 * image[i][j].rgbtGreen +
+            sepiaGreen = round(0.349 * image[i][j].rgbtRed + 0.686 * image[i][j].rgbtGreen + 
                                0.168 * image[i][j].rgbtBlue);
-            sepiaBlue = round(0.272 * image[i][j].rgbtRed +
-                              0.534 * image[i][j].rgbtGreen +
+            sepiaBlue = round(0.272 * image[i][j].rgbtRed + 0.534 * image[i][j].rgbtGreen + 
                               0.131 * image[i][j].rgbtBlue);
 
+            // Cap values at 255 if they exceed it, else keep them as computed sepia values as per spec
             image[i][j].rgbtRed = (sepiaRed < 255) ? (sepiaRed) : (255);
             image[i][j].rgbtGreen = (sepiaGreen < 255) ? (sepiaGreen) : (255);
             image[i][j].rgbtBlue = (sepiaBlue < 255) ? (sepiaBlue) : (255);
@@ -51,9 +63,15 @@ void sepia(int height, int width, RGBTRIPLE image[height][width]) {
 }
 
 // Reflect image horizontally
-void reflect(int height, int width, RGBTRIPLE image[height][width]) {
-    for (int i = 0; i < height; i++) {
-        for (int j = 0; j < width / 2; j++) {
+void reflect(int height, int width, RGBTRIPLE image[height][width])
+{
+    // Iterate over each row of pixels
+    for (int i = 0; i < height; i++)
+    {
+        // Iterate over half the pixels in row
+        for (int j = 0; j < width / 2; j++)
+        {
+            // Swap subpixel intensities with those of pixel at the same index from the end
             swap_e(image[i][j].rgbtRed, image[i][width - j - 1].rgbtRed);
             swap_e(image[i][j].rgbtGreen, image[i][width - j - 1].rgbtGreen);
             swap_e(image[i][j].rgbtBlue, image[i][width - j - 1].rgbtBlue);
@@ -63,44 +81,59 @@ void reflect(int height, int width, RGBTRIPLE image[height][width]) {
 }
 
 // Blur image
-void blur(int height, int width, RGBTRIPLE image[height][width]) {
+void blur(int height, int width, RGBTRIPLE image[height][width])
+{
     RGBTRIPLE blurImage[height][width];
 
-    // iterate over each pixel in the image
-    for (int i = 0; i < height; i++) {
-        for (int j = 0; j < width; j++) {
-            // store sum of neighbor pixel intensities in ints
+    // Iterate over each row of pixels
+    for (int i = 0; i < height; i++)
+    {
+        // Iterate over each pixel in row
+        for (int j = 0; j < width; j++)
+        {
+            // Store sum of neighbor pixel intensities
             int sumRed = 0, sumGreen = 0, sumBlue = 0;
-            // keep track of valid neighbors
+
+            // Keep track of valid neighbors
             int neighborCount = 0;
 
-            // iterate over 3x3 grid around each pixel, add pixel intensities
-            // skip if outside bound
-            for (int k = -1; k <= 1; k++) {
-                // jump to next iteration if cell row is out of bounds
-                if (i + k < 0 || i + k >= height) {
+            // Iterate over 3x3 grid around each pixel, add pixel intensities
+            // Skip if outside bound
+            for (int k = -1; k <= 1; k++)
+            {
+                // Jump to next iteration if cell row is out of bounds
+                if (i + k < 0 || i + k >= height)
+                {
                     continue;
                 }
-                for (int l = -1; l <= 1; l++) {
-                    // jump to next iteration if cell column is out of bounds
-                    if (j + l < 0 || j + l >= width) {
+                for (int l = -1; l <= 1; l++)
+                {
+                    // Jump to next iteration if cell column is out of bounds
+                    if (j + l < 0 || j + l >= width)
+                    {
                         continue;
                     }
                     neighborCount++;
                     sumRed += image[i + k][j + l].rgbtRed;
-                        sumGreen += image[i + k][j + l].rgbtGreen;
-                        sumBlue += image[i + k][j + l].rgbtBlue;
+                    sumGreen += image[i + k][j + l].rgbtGreen;
+                    sumBlue += image[i + k][j + l].rgbtBlue;
                 }
             }
 
+            // Compute average subpixel intensities from sum of neighbor intensities
+            // rounded to nearest int
             blurImage[i][j].rgbtRed = round(sumRed / (float)neighborCount);
             blurImage[i][j].rgbtGreen = round(sumGreen / (float)neighborCount);
             blurImage[i][j].rgbtBlue = round(sumBlue / (float)neighborCount);
         }
     }
-    // copies subpixel values from blurImage to image
-    for (int i = 0; i < height; i++) {
-        for (int j = 0; j < width; j++) {
+    // Copies subpixel values from blurImage to image
+    // Iterating over each row in pixel grid
+    for (int i = 0; i < height; i++)
+    {
+        // Iterating over each pixel
+        for (int j = 0; j < width; j++)
+        {
             image[i][j].rgbtRed = blurImage[i][j].rgbtRed;
             image[i][j].rgbtGreen = blurImage[i][j].rgbtGreen;
             image[i][j].rgbtBlue = blurImage[i][j].rgbtBlue;
