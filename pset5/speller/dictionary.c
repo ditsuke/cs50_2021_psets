4003f49de88b9a4f1175d532566b80131de19b0e
dst27 <72784348+dst27@users.noreply.github.com>
2021-03-02 11:58:22 +0530

    pset5/speller - improve styling
    Improved comments and styling.

--- pset5/speller/dictionary.c
+++ pset5/speller/dictionary.c
@@ -10,7 +10,8 @@
 #include <stdint.h>
 
 // Represents a node in a hash table
-typedef struct node {
+typedef struct node
+{
     char word[LENGTH + 1];
     struct node *next;
 } node;
@@ -28,23 +29,30 @@ node *listend[N];
 int dictsize = 0;
 
 // Returns true if word is in dictionary, else false
-bool check(const char *word) {
+bool check(const char *word)
+{
     // Hash value for passed word
     int hashc = hash(word);
     // Bool to tell if word is in dictionary
     bool isIn = false;
     node *llp = table[hashc];
-    while (true) {
+    while (true)
+    {
         int comp = strcasecmp(word, llp->word);
-        if (comp == 0) {
+        if (comp == 0)
+        {
             // Break if found
             isIn = true;
             break;
-        } else if (comp > 0 && llp->next != NULL) {
+        }
+        else if (comp > 0 && llp->next != NULL)
+        {
             // Advance linked list pos ptr, jump to next iteration
             llp = llp->next;
             continue;
-        } else {
+        }
+        else
+        {
             // Break if lexicographic position of word has passed
             break;
         }
@@ -53,12 +61,15 @@ bool check(const char *word) {
 }
 
 // Hashes word to a number using the djb2 algo
-unsigned int hash(const char *word) {
+unsigned int hash(const char *word)
+{
     unsigned long hash = 5381;
     int c;
 
-    while ((c = *word++)) {
-        c = ((c >= 97 && c <=122) ? c - 97 : (c >= 65 && c <=90) ? c - 65 : c);
+    while ((c = *word++))
+    {
+        c = ((c >= 97 && c <= 122) ? c - 97 : (c >= 65 && c <= 90) ? c - 65
+             : c);
         // hash * 33 + c (<<5 uses left shift to achieve this more efficiently)
         hash = ((hash << 5) + hash) + c;
     }
@@ -67,59 +78,82 @@ unsigned int hash(const char *word) {
 }
 
 // Loads dictionary into memory, returning true if successful, else false
-bool load(const char *dictionary) {
-    FILE *dictstr;
-    dictstr = fopen(dictionary, "r");
+bool load(const char *dictionary)
+{
+    // Dictionary file stream
+    FILE *dictstr = fopen(dictionary, "r");
+
     // Return false if unable to open dictionary file
-    if (dictstr == NULL) {
+    if (dictstr == NULL)
+    {
         return false;
     }
 
     // Initialise first nodes and listend ptrs
-    for (int i = 0; i < N; ++i) {
+    for (int i = 0; i < N; ++i)
+    {
         table[i] = calloc(1, sizeof(node));
         listend[i] = table[i];
     }
-
+    // Stores "current" word
     char cword[LENGTH + 1];
-    // Stores fscanf return, EOF when file has been read
+    // Stores fscanf return
     int word_read = 0;
-    do {
+
+    do
+    {
+        // Read a word from dict stream
         word_read = fscanf(dictstr, "%s\n", cword);
-        if (word_read == EOF) {
+
+        // Break if EOF reached
+        if (word_read == EOF)
+        {
             break;
         }
+
         // Increment dictionary size
         ++dictsize;
         // Stores word hash
         unsigned int chash = hash(cword);
         strcpy(listend[chash]->word, cword);
+
         // Allocate memory for next node in linked list
         listend[chash]->next = calloc(1, sizeof(node));
         // Advance ptr
         listend[chash] = listend[chash]->next;
-    } while (true);
+    } 
+    while (true);
 
     fclose(dictstr);
     return true;
 }
 
 // Returns number of words in dictionary if loaded, else 0 if not yet loaded
-unsigned int size(void) {
+unsigned int size(void)
+{
     return dictsize;
 }
 
 // Unloads dictionary from memory, returning true if successful, else false
-bool unload(void) {
-    for (int i = 0; i < N; ++i) {
+bool unload(void)
+{
+    // Iterate over hash table nodes
+    for (int i = 0; i < N; ++i)
+    {
         node *ll = table[i];
         node *llr;
-        while (true) {
+
+        // Transverse linked list and keep freeing memory until end of list
+        while (true)
+        {
             llr = ll;
-            if (ll->next != NULL) {
+            if (ll->next != NULL)
+            {
                 ll = ll->next;
                 free(llr);
-            } else {
+            }
+            else
+            {
                 free(llr);
                 break;
             }
