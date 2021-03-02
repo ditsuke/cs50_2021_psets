e1dd0565f01035a13ffbd036cb710260b73f7db3
dst27 <72784348+dst27@users.noreply.github.com>
2021-03-02 22:07:01 +0530

    pset6/readabilty - improve design
    Switched to using re.findall to compute letter_count

--- pset6/readability/readability.py
+++ pset6/readability/readability.py
@@ -20,11 +20,7 @@ def main():
 def ColemanLiau_I(text):
     wor = len(text.split(" "))                  # Number of words
     sen = len(re.split("\.|\?|!", text)) - 1    # Number of sentences
-
-    # Count letters in text string
-    let = 0
-    for char in text:
-        let += 1 if char.isalpha() else 0
+    let = len(re.findall("[A-Za-z]", text))     # Number of letters
 
     # Compute index using the Coleman-Liau formula
     index = 0.0588 * let * 100 / wor - 0.296 * sen * 100 / wor - 15.8
