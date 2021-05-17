cf88576cc7b202717fa43d9e8c6cb02ac88c87c8
dst27 <72784348+dst27@users.noreply.github.com>
2021-05-17 19:57:21 +0530

    pset6/dna - bugfix and refactor
    Was printing "No Match" after each failed match because of extra indentation

--- pset6/dna/dna.py
+++ pset6/dna/dna.py
@@ -1,68 +1,76 @@
-import sys, csv
+import sys
+import csv
+
 
 def main():
     argc = len(sys.argv)
 
-    # Return with code 1 if incorrect usage/arg count
+    # Check for incorrect usage
     if argc != 3:
         print("Usage: ./dna.py <db_file> <sequence_file>")
         sys.exit(1)
+    else:
+        db_file = sys.argv[1]
+        sequence_file = sys.argv[2]
+
+    # Get STRs and their consecutive occurences in sequence
+    with open(db_file) as db_stream:
+        db_csv = csv.reader(db_stream)
+        STRs = next(db_csv)[1::]
+        str_occurence = {}
+        for i in range(len(STRs)):
+            str_occurence[STRs[i]] = str_repetitions(STRs[i], sequence_file)
 
 
-    # Get strs and str consecutive occurence in sequence
-    with open(sys.argv[1]) as dbstream:
-        csvdb = csv.reader(dbstream)
-        header = next(csvdb)
-        strs = []
-        str_occ = {}
-        for i in range(len(header) - 1):
-            strs.append(header[i + 1])
-            str_occ[header[i + 1]] = str_reps(header[i + 1], sys.argv[2])
-
-    # Get csv database as a list of dicts ("person"s)
-    with open(sys.argv[1]) as dbstream:
-        csvdb = csv.DictReader(dbstream)
+    # Get CSV database as a list of people
+    with open(db_file) as db_stream:
+        db_csv_dict = csv.DictReader(db_stream)
         people = []
-        for row in csvdb:
+        for row in db_csv_dict:
             people.append(row)
 
     # Check for matches of sequence STRs for each person in people
     for person in people:
-        match_no = 0
-        for str_ in strs:
-            if int(person[str_]) == str_occ[str_]:
-                match_no += 1
-        if match_no == len(strs):
+        str_matches = 0
+        for str_ in STRs:
+            if int(person[str_]) == str_occurence[str_]:
+                str_matches += 1
+        if str_matches == len(STRs):
             print(person['name'])
             sys.exit(0)
 
-        print("No match")
+    print("No match")
 
-# Max consecutive occurrence of a STR
-def str_reps(str_seq, seqfile):
-    str_len = len(str_seq)
-    repcount = 0
-    consecutive = 0
+
+# Return the number of consecutive occurences of the STR (str_seq) in the Sequence file (seq_file)
+def str_repetitions(str_sequence, sequence_file):
+    str_length = len(str_sequence)
+    repetitions = 0
+    consecutive_matches = 0
     pos = 0
 
-    with open(seqfile) as seqstr:
-        buff = seqstr.read()
+    # Read sequence_file into a buffer string
+    with open(sequence_file) as sequence_stream:
+        buffer = sequence_stream.read()
+        buffer_len = len(buffer)
+
     i = 0
-    while i < len(buff):
-        cc = buff[i]
+    while i < buffer_len:
+        cc = buffer[i]
         i += 1
-        if cc == str_seq[pos]:
-           pos += 1
-           if pos == str_len:
-               repcount += 1
-               if repcount > consecutive:
-                   consecutive = repcount
-               pos = 0
+        if cc == str_sequence[pos]:
+            pos += 1
+            if pos == str_length:
+                repetitions += 1
+                if repetitions > consecutive_matches:
+                    consecutive_matches = repetitions
+                pos = 0
         else:
             i = i - pos
             pos = 0
-            repcount = 0
+            repetitions = 0
 
-    return consecutive
+    return consecutive_matches
+
 
 main()
